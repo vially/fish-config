050b12eb076e7df04c9bd669606d48eab0482ac6
Valentin Hăloiu <vially.ichb@gmail.com>
2014-06-15 09:21:00 +0700

    Improve prompt display

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,7 +1,26 @@
-function fish_prompt
-	# virtualenv
-	if set -q VIRTUAL_ENV
-		echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-	end
-	printf '%s@%s %s%s%s%s $ ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt)
+function fish_prompt --description "Write out the prompt"
+    # Just calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    end
+
+    if not set -q __fish_prompt_hostcolor
+        set -g __fish_prompt_hostcolor (set_color normal)
+    end
+
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    set -g __fish_prompt_cwd (set_color normal)
+    if set -q __fish_color_cwd
+        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    end
+
+    # virtualenv
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+    end
+
+    echo -s "$USER" @ "$__fish_prompt_hostcolor" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" (__fish_git_prompt) ' $ '
 end
