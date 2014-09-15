f76c223b2a9169565d92e68685b1ad971eceae1b
Valentin Hăloiu <vially.ichb@gmail.com>
2014-09-15 14:39:02 +0700

    Remove fixnpmpath function (use direnv instead)

--- functions/fixnpmpath.fish
+++
@@ -1,6 +0,0 @@
-function fixnpmpath --description 'Add `npm bin` to PATH'
-  set npm_bin_path (npm bin)
-  if begin; test -e $npm_bin_path; and not contains $npm_bin_path $PATH; end
-    set -x PATH $npm_bin_path $PATH
-  end
-end
