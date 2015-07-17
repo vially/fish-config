b56c767343ec9519f55a3aa19f50a1216c690754
Valentin Hăloiu <vially.ichb@gmail.com>
2015-07-17 07:55:49 +0300

    dotenv now takes an optional filename argument

--- functions/dotenv.fish
+++ functions/dotenv.fish
@@ -1,6 +1,11 @@
-function dotenv --description 'Load environment variables from the .env file'
-  if test -e .env
-    for line in (cat .env)
+function dotenv --description 'Load environment variables from .env file'
+  set -l envfile ".env"
+  if [ (count $argv) -gt 0 ]
+    set envfile $argv[1]
+  end
+
+  if test -e $envfile
+    for line in (cat $envfile)
       set -xg (echo $line | cut -d = -f 1) (echo $line | cut -d = -f 2-)
     end
   end
