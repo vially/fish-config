ba6fba1a6dce917f28496da46b06c33304f74cee
Valentin Hăloiu <vially.ichb@gmail.com>
2015-07-21 04:33:16 +0300

    Cleanup aliases and abbreviations

--- config.fish
+++ config.fish
@@ -4,40 +4,29 @@ source ~/.config/fish/modules/chruby/auto.fish                   # chruby auto-a
 source ~/.config/fish/modules/localconfig/localconfig.fish       # load local machine config from local.fish
 source ~/.config/fish/modules/direnv/hook.fish                   # direnv integration
 
+## Variables
+
+# Disable greeting
+set fish_greeting
+
 # git prompt config
 set __fish_git_prompt_showdirtystate 1
 set __fish_git_prompt_showupstream verbose
 set __fish_git_prompt_color_branch blue
 set __fish_git_prompt_color_dirtystate red
 
-# Disable greeting
-set fish_greeting
-
-# Enable vi-mode
-vi_mode
-
-# Variables
+# npm
 set -x NPM_PACKAGES ~/.local/share/npm
 set -x MANPATH $NPM_PACKAGES/share/man $MANPATH
 set -x NODE_PATH $NPM_PACKAGES/lib/node_modules $NODE_PATH
+
+# generic
 set -x EDITOR vim                                                   # vim FTW
 set -x PATH ~/.local/bin $GOPATH/bin $NPM_PACKAGES/bin $PATH        # set PATH
 set -x GNUPGHOME ~/.config/gnupg
 
 # Aliases
-alias uncrustify-all='uncrustify -c uncrustify.cfg -l OC --no-backup (ffind "(\.h|\.m)\$")'
 alias pt='pt -S'
-alias t='task'
-alias sctl='sudo systemctl'
-alias sctld='sudo systemctl daemon-reload'
-alias sctli='sudo systemctl status'
-alias sctlr='sudo systemctl restart'
-alias sctlk='sudo systemctl stop'
-alias scu='systemctl --user'
-alias scud='systemctl --user daemon-reload'
-alias scuk='systemctl --user stop'
-alias scui='systemctl --user status'
-alias scur='systemctl --user restart'
 alias vim='nvim'
 
 # Abbreviations
@@ -53,3 +42,16 @@ abbr -a gcl='gcloud compute instances list'
 abbr -a gcdel='gcloud compute instances delete'
 abbr -a tl='tarsnap --list-archives'
 abbr -a vish='vim ~/.config/fish/config.fish'
+abbr -a sctl='sudo systemctl'
+abbr -a sctld='sudo systemctl daemon-reload'
+abbr -a sctli='sudo systemctl status'
+abbr -a sctlr='sudo systemctl restart'
+abbr -a sctlk='sudo systemctl stop'
+abbr -a scu='systemctl --user'
+abbr -a scud='systemctl --user daemon-reload'
+abbr -a scuk='systemctl --user stop'
+abbr -a scui='systemctl --user status'
+abbr -a scur='systemctl --user restart'
+
+# Enable vi-mode
+vi_mode
