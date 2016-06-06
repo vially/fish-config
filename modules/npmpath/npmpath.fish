ab616a59d36c0e69d6726cd638df2515d93b75ea
Valentin Hăloiu <vially.ichb@gmail.com>
2016-06-06 16:27:06 +0300

    Remove leftover npm module files

--- modules/npmpath/npmpath.fish
+++
@@ -1,7 +0,0 @@
-function __autoset_npm_path --on-variable PWD --description 'Add `npm bin` to PATH on directory change'
-  status --is-command-substitution; and return
-
-  fixnpmpath
-end
-
-__autoset_npm_path
